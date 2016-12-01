02bcc9fa5843ca480617cf9dfadafe4f0bd0b7a9
Carolina <krito_j_g@utp.edu.co>
2016-12-01 12:19:28 -0500

    Lee archvios del csv y lo almacena en listas Error con los tamaños de listas (en algunas imágenes no está encontrando caras)

--- ReconocimientoFacial/Reconocedor.py
+++ ReconocimientoFacial/Reconocedor.py
@@ -1,7 +1,63 @@
 import cv2
 import sys
+import csv
+import numpy as np
+from PIL import Image
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') #clasificadores Haar para detectar caras
+
+def train(x,labels):
+	images = []
+	minSize = np.inf
+	index = 0
+	for image_path in x: #imagenes
+
+		index += 1
+		image_pil = Image.open(image_path).convert('L')
+		img = np.array(image_pil, 'uint8')
+		faces = FindFaces(img)
+
+		if (len(faces) == 0): # no faces
+			#images.append(np.array([-1]))
+			labels.pop(index-1)
+		else:
+			for (x, y, w, h) in faces:
+				face = img[y: y + h, x: x + w]
+				if (face.size < minSize):
+					minSize = face.size
+					min_img = face
+				images.append(face)#solo nos interesa la cara
+
+	height, width = min_img.shape[:2]
+	#deben tener el mismo tamaño para entrenar el algoritmo
+	"""for i in range(0,len(images)):
+		if (images[i].any() == -1):
+			print ("remove")
+			images.remove(i)
+			labels.remove(i)
+		else:
+			if (images[i].shape[:2] != min_img.shape[:2]):
+				images[i] = cv2.resize(images[i], (width, height), interpolation = cv2.INTER_CUBIC)
+	"""
+	print ("images : ", len(images))
+	print ("labels : ", len(labels))
+
+	#recognizer = cv2.face.createEigenFaceRecognizer()
+	#recognizer.train(images,np.array(labels))
+
+def getInfo(file_name):
+	images = []
+	labels = []
+	f = open(file_name, 'r') # opens the csv file
+	try:
+		reader = csv.reader(f)
+		for row in reader:
+			images.append(row[0].split(";")[0])
+			labels.append(int(row[0].split(";")[1]))
+	finally:
+	    f.close()
+
+	return images,labels
 
 def FindFaces(image):
 
@@ -24,15 +80,19 @@ def ShowFaces(faces, image):
 	cv2.destroyAllWindows()
 
 def main():
-	if len(sys.argv) != 2:
+	if len(sys.argv) != 3:
+		print ("uso: python3 Reconocedor.py <Imagen> <rutaCSV>")
 		sys.exit(1)
 
 	image_dir = sys.argv[1]
+	file_name = sys.argv[2]
 	image = cv2.imread(image_dir)
 	img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # se convierte a escala de grises
 
-	faces = FindFaces(img_gray)
-	ShowFaces(faces,image)
+	# retorna la informacion del archivo csv en dos arreglos
+	x,y = getInfo(file_name)
+
+	train(x,y)
 
 if __name__ == "__main__":
     main()
