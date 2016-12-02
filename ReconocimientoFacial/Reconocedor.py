bac2452c8a91ed468e8768d69546a95aae7bddac
Carolina <krito_j_g@utp.edu.co>
2016-12-01 20:29:03 -0500

    Agrega fotos Vane y Caro

--- ReconocimientoFacial/Reconocedor.py
+++ ReconocimientoFacial/Reconocedor.py
@@ -2,54 +2,48 @@ import cv2
 import sys
 import csv
 import numpy as np
-#from PIL import Image
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') #clasificadores Haar para detectar caras
 
+def normalizeImage(image):
+	if (image.shape[:2] > (512,512)):
+		return cv2.resize(image, (512, 512))#,interpolation = cv2.INTER_CUBIC)
+
 def train(x,l):
 	images = []
+	labels = []
 	minSize = np.inf
-	#index = 0
+	index = 0
 	for image_path in x: #imagenes
-		#index += 1
+		index += 1
 		img = cv2.imread(image_path)
-		#image_pil = Image.open(image_path).convert('L')
-		#img = np.array(image_pil, 'uint8')
+		img = normalizeImage(img)
 		faces = FindFaces(img)
 
-		if (len(faces) == 0): # si no ha detectado caras
-			images.append(img)
-		for (x, y, w, h) in faces:
-			face = img[y: y + h, x: x + w]
-			if (face.size < minSize):
-				minSize = face.size
-				min_img = face
-			images.append(face)#solo nos interesa la cara
+		if (len(faces) != 0): # si ha detectado caras
+			for (x, y, w, h) in faces:
+				face = img[y: y + h, x: x + w]
+				if (face.size < minSize):
+					minSize = face.size
+					min_img = face
+				images.append(face)#solo nos interesa la cara
+				labels.append(l[index-1])
 
 	height, width = min_img.shape[:2]
-	print ("min_size:", minSize)
 
 	#deben tener el mismo tamaño para entrenar el algoritmo
 	for i in range(0,len(images)):
-		if (images[i].any() == -1):
-			print ("remove")
-			images.remove(i)
-			labels.remove(i)
-		else:
-			if (images[i].shape[:2] != min_img.shape[:2]):
-				images[i] = cv2.resize(images[i], (width, height), interpolation = cv2.INTER_CUBIC)
+		if (images[i].shape[:2] != min_img.shape[:2]):
+			images[i] = cv2.resize(images[i], (width, height))#, interpolation = cv2.INTER_CUBIC)
 
-	#print ("images : ", len(images))
-	#print ("labels : ", len(l))
-
-	"""for image in images:
-		#cv2.imshow("Detectados", image)
-		#cv2.waitKey(0)
-		print ("guat?:", image.size)"""
-
-
+	for image in images:
+		cv2.imshow("Entrenados", image)
+		cv2.waitKey(50)
+		#print ("guat?:", image.size)
+	cv2.destroyAllWindows()
 	recognizer = cv2.face.createEigenFaceRecognizer()
-	recognizer.train(images,np.array(l))
+	recognizer.train(images,np.array(labels))
+	#recognizer.save("recognizer.xml")
 	return recognizer, min_img
 
 def getInfo(file_name):
@@ -82,7 +76,7 @@ def ShowFaces(faces, image):
 	for (x, y, w, h) in faces:
 		cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0),2)
 
-	cv2.imshow("Detectados", image)
+	cv2.imshow("", image)
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
 
@@ -94,6 +88,7 @@ def main():
 	image_dir = sys.argv[1]
 	file_name = sys.argv[2]
 	image = cv2.imread(image_dir)
+	image = normalizeImage(image)
 	img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # se convierte a escala de grises
 
 	# retorna la informacion del archivo csv en dos arreglos
@@ -104,12 +99,11 @@ def main():
 
 	#busca en la imagen ingresada si hay un rostro conocido
 	faces = FindFaces(img_gray) #busca solo las caras
-	print ("faces:", len(faces))
+	print ("faces : ", len(faces))
 	for (x, y, w, h) in faces:
 		face = img_gray[y: y + h, x: x + w]
-		print ("face: ", face.size)
 		if (face.shape[:2] != min_img.shape[:2]):
-			face = cv2.resize(face, (width, height), interpolation = cv2.INTER_CUBIC)
+			face = cv2.resize(face, (width, height))#, interpolation = cv2.INTER_CUBIC)
 
 		cv2.imshow("uhmm", face)
 		cv2.waitKey(0)
@@ -117,6 +111,14 @@ def main():
 		id, conf = recognizer.predict(face)
 		print ("id: ", id)
 		print ("conf: ", conf)
+
+		if (conf > 50):#si encuentra a alguien con un parecido mayor a 60%
+			#pinta el triangulo en su cara
+			cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0),2)
+
+	#muestra la imagen analizada
+	cv2.imshow("Detectados", image)
+	cv2.waitKey(0)
 
 if __name__ == "__main__":
     main()
